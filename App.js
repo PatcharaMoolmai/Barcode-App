0434270832a02cdd4c01329469b4afefcfaf9ebe
Patchara Moolmai <klong.pokemon@gmail.com>
2020-10-24 23:23:26 +0700

    Create Project

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Logo</Text>
+      <Text>Username</Text>
+      <Text>Password</Text>
+      {/* <TextInput  
+        placeholder="Username"
+        styles={{height :40, borderColor: "gray", borderWidth:1 }}
+        onChangeText={text=>onChangeText(text)}
+        value={value}></TextInput> */}
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +25,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  
 });
